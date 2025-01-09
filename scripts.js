2b9d5436d33f2fb682258f06de8acb0989b66368
Joshua Boothby <Mona.Lisa@example.com>
2025-01-09 17:23:40 -0600

    added band array of objects

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let bands = [
+  { name: "The Beatles", genre: "Rock", formedYear: 1960, active: false },
+  { name: "Led Zeppelin", genre: "Rock", formedYear: 1968, active: false },
+  { name: "Pink Floyd", genre: "Rock", formedYear: 1965, active: false },
+  {
+    name: "The Black Dahlia Murder",
+    genre: "Melodic Death Metal",
+    formedYear: 2001,
+    active: true,
+  },
+];
